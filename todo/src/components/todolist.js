d0ebd68adb89f7ed0f74209247716b87ec32da1e
Tom Bauer <tbauer1898@gmail.com>
2020-09-08 16:20:48 -0500

    Add item working.

--- todo/src/components/todolist.js
+++ todo/src/components/todolist.js
@@ -1,15 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 
 import { initialState, todoReducer } from '../reducers/index'
 
+// dispatch takes in an action object, and calls in the reducer function
+// with state and that action
+
 const TodoList = () => {
-    const [newTodo, setNewTodo] = useState("")
+    const [state, dispatch] = useReducer(todoReducer, initialState) // two args = reducer, initialState
+    // const [Todo, setTodos] = useState("")
+    const [newTodoText, setNewTodoText] = useState('')
+
+    const handleChanges = e => {
+        setNewTodoText(e.target.value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        dispatch({ type: 'ADD_TODO',
+        payload: newTodoText})
+
+    }
 
     return (
         <div>
-            {initialState.map(task => (
+        
+            <input
+            type = 'text'
+            name = 'newTodoText'
+            placeholder='Add a Todo'
+            onChange={handleChanges}
+            />
+            <button onClick={handleSubmit}>Add ToDo</button>
+            <button>Clear Completed Tasks</button>
+            {state.todos.map(task => (
                 <div>
-                    <h3>{task.item}</h3>
+                    <h4>{task.item}</h4>
                 </div>
             ))}
         </div>
--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,20 +1,35 @@
-export const initialState = [{
-    item: 'Learn Redux',
-    completed: false,
-    id: new Date()
-},
-{
-    item: 'Review Reducers',
-    completed: false,
-    id: new Date()
-}]
+export const initialState = {
+    todos: [ {
+        item: 'Learn Redux',
+        completed: false,
+        id: Math.random()
+    },
+    {
+        item: 'Workout',
+        completed: false,
+        id: Math.random()
+    }
+
+]}
+// reducers are functions that take in two objects, state and action
+// and they reduce them down to a single object, which is the new state
+
+// EVENT: CLICK ON TASK to toggle completed
+// {type: "TOGGLE_COMPLETED"}
 
 export const todoReducer = (state, action) => {
-    switch (action.type) {
-
+    switch(action.type) {
         case "ADD_TODO":
-            return {...state, item: ''}
-        default:
-            return state
+            const newTodo = {
+                item: action.payload,
+                completed: false,
+                id: Math.random()
+            }
+            return {
+                ...state, 
+                todos: [...state.todos, newTodo]
+            }
+        
     }
 }
+
